daa013f11ed9425f7c410541c80c1a595d7318da
Lauren Powers <laurenpowers@Laurens-Air.lan>
2022-12-18 15:37:39 -0500

    change gradient and nav font

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,23 +1,35 @@
-import "./styles/Nav.css"
-import { NavLink } from "react-router-dom"
-import cassette from "../images/cassette-tape-no-title.png"
+import "./styles/Nav.css";
+import { NavLink } from "react-router-dom";
+import cassette from "../images/cassette-tape-no-title.png";
 
 function Nav() {
   return (
     <div className="nav-bar-component">
       <div className="title-logo">
-      <NavLink to="/"><img src={cassette} alt="cassette logo" className="cassette-logo" /></NavLink><h1 className="b-side-music">B-Side Music</h1>
+        <NavLink to="/">
+          <img src={cassette} alt="cassette logo" className="cassette-logo" />
+        </NavLink>
+        <p className="b-side-music">B-Side Music</p>
       </div>
       <div className="nav">
-          <div className="navlink" activeClassName="active"><NavLink to="/">Home</NavLink></div>
-          <div className="navlink" activeClassName="active"><NavLink to="/artists">Artists</NavLink></div>
-          <div className="navlink" activeClassName="active"><NavLink to="/albums">Albums</NavLink></div>
-          <div className="navlink" activeClassName="active"><NavLink to="/genres">Genres</NavLink></div>
-        <div className="navlink" activeClassName="active"><NavLink to="/about">About</NavLink></div>
+        <div className="navlink" activeClassName="active">
+          <NavLink to="/">Home</NavLink>
+        </div>
+        <div className="navlink" activeClassName="active">
+          <NavLink to="/artists">Artists</NavLink>
+        </div>
+        <div className="navlink" activeClassName="active">
+          <NavLink to="/albums">Albums</NavLink>
+        </div>
+        <div className="navlink" activeClassName="active">
+          <NavLink to="/genres">Genres</NavLink>
+        </div>
+        <div className="navlink" activeClassName="active">
+          <NavLink to="/about">About</NavLink>
+        </div>
       </div>
- </div>
-    
-  )
+    </div>
+  );
 }
 
-export default Nav
+export default Nav;
