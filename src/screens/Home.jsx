dc4bdf126942eaff956e5f4f367bf1194c456e1b
Lauren Powers <laurenpowers@Laurens-Air.lan>
2022-12-30 10:52:22 -0500

    add text to homepage

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -1,15 +1,17 @@
-import "./styles/Home.css"
-
+import "./styles/Home.css";
 
 function Home() {
   return (
     <>
-
       <h1>B-SIDE MUSIC</h1>
-      <h1 className="artist-name">
-        A database of lesser known musicians and artists.
+      <h1 className="about">
+        B-Side Music is a database here to represent music from artists that
+        need to be heard. Find and search for artists of varying genres and
+        discover songs to add to your collections. Do you have an artist you
+        want represented? We can add them to our expanding list and help get
+        them discovered. Flip your cassette to the B-Side and get ready to hear
+        something new.
       </h1>
-
     </>
   );
 }
